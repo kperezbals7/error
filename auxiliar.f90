9d8236eede4eea0a610175fffd8ca21a6b7deaf7
Katerina Pérez Balskute <kperezb@correo.ugr.es>
2026-04-19 09:25:36 +0200

    Refactor error calculation in aux program
    Updated the error calculation to use a corrected value for 'errores'.

--- auxiliar.f90
+++ auxiliar.f90
@@ -4,7 +4,7 @@ program aux
     lambda = 512E-9
     l = 1.1
     b = 3.50E-03
-    errores = 0.001
+    errores = 0.001 / sqrt(12)
 
     a = (2*l*lambda) / b
     errora = sqrt(((2*lambda*errores)/b)**2+((2*l*lambda*errores)/b**2)**2)
