857ebe8e8285ba0335089f81e2e008132beea3b4
Katerina Pérez Balskute <kperezb@correo.ugr.es>
2026-04-19 09:23:26 +0200

    Add files via upload

---
+++ auxiliar.f90
@@ -0,0 +1,15 @@
+program aux
+    real lambda, b, errores, l, a, errora
+
+    lambda = 512E-9
+    l = 1.1
+    b = 3.50E-03
+    errores = 0.001
+
+    a = (2*l*lambda) / b
+    errora = sqrt(((2*lambda*errores)/b)**2+((2*l*lambda*errores)/b**2)**2)
+
+    write (*,*) a
+    write (*,*) errora
+
+end program
